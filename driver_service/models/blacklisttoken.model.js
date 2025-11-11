2f4ca332931182583ebfc1c6a11fba00fe81a204
nirvair09 <chandradeep262003@gmail.com>
2025-11-12 00:09:10 +0530

    Add blacklist token model for driver and user services

---
+++ driver_service/models/blacklisttoken.model.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+
+const blacklistTokenSchema = new mongoose.Schema({
+    token: {
+        type: String,
+        required: true
+    },
+    creratedAt: {
+        type: Date,
+        default: Date.now,
+        expires: 3600 // 1 hour
+    }
+}, {
+    timestamps: true,
+});
+
+
+module.exports = mongoose.model("blacklisttoken", blacklistTokenSchema);   
