f564ade2c6c5b19de9e6c57c9a04eca08192f559
nirvair09 <chandradeep262003@gmail.com>
2025-11-14 23:44:46 +0530

    Refactor ride routes; streamline route definitions and ensure proper middleware usage

--- ride_service/routes/ride.routes.js
+++ ride_service/routes/ride.routes.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const { userAuth, driverAuth } = require("../middleware/auth.middleware");
+const { createRide, acceptRide } = require("../controllers/ride.controller");
 const router = express.Router();
 
-
+router.post("/create-ride", userAuth, createRide);
+router.post("/accept-ride", driverAuth, acceptRide);
 
 module.exports = router;
